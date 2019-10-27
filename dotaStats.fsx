1f7ab632ac65f70016b9b96d7b18fe73fb51fd7a
J2ghz <j2.00ghz@gmail.com>
2019-10-27 17:12:51 +0100

    better text

--- dotaStats.fsx
+++ dotaStats.fsx
@@ -1,43 +1,47 @@
-open System.Globalization
 (*** hide ***)
 #load "packages/FsLab/Themes/DefaultWhite.fsx"
 #load "packages/FsLab/FsLab.fsx"
 
-(**
-
-Welcome to FsLab journal
-========================
-
-Sample experiment
------------------
-
-We start by referencing `Deedle` and `XPlot.GoogleCharts` libraries and then we
-load the contents of *this* file:
-*)
-
 open System
 open Deedle
-open System.IO
+open System.Globalization
 open XPlot.GoogleCharts
 open FSharp.Data
+(**
 
+Dota 2 Data
+========================
+
+Games per week
+-----------------
+
+We'll need to conver timestamps to dates and dates to week numbers
+*)
 let toDateTime (timestamp:int) =
   let start = DateTime(1970,1,1,0,0,0,DateTimeKind.Utc)
   start.AddSeconds(float timestamp).ToLocalTime()
 
+let toWeekNumber (d:DateTime) = 
+  CultureInfo.CurrentCulture.DateTimeFormat.Calendar.GetWeekOfYear(
+    d,
+    CalendarWeekRule.FirstFourDayWeek,
+    DayOfWeek.Monday)
+(**
+Load the data, and transform it to have just number of games per "year/week"
+*)
 type OpenDotaPlayerData = JsonProvider<"https://api.opendota.com/api/players/71050296/matches">
 
 let data = OpenDotaPlayerData.Load "https://api.opendota.com/api/players/71050296/matches"
-          |> Array.map (fun m -> m.StartTime |> toDateTime) 
-          |> Array.map (fun d -> d.Year,CultureInfo.CurrentCulture.DateTimeFormat.Calendar.GetWeekOfYear(d,CalendarWeekRule.FirstFourDayWeek,DayOfWeek.Monday))
+          |> Array.map (fun m -> 
+            let d = m.StartTime |> toDateTime
+            d.Year,toWeekNumber d)
           |> Array.countBy id
           |> Array.where (fun ((y,_),_) -> (y = 2019))
           |> Array.map (fun ((y,w),c) -> (sprintf "%i/%i" y w , c))
           |> Array.toList
 
 (**
-Now, we split the contents of the file into words, count the frequency of
-words longer than 3 letters and turn the result into a Deedle series:
+The data in the table:
 *)
 (*** define-output:series ***)
 let dataSeries =
@@ -47,7 +51,7 @@ dataSeries
 (*** include-it:series ***)
 
 (**
-Finally, we can take the same 6 words and call `Chart.Column` to see them in a chart:
+And in a chart:
 *)
 (*** define-output:chart ***)
 dataSeries
