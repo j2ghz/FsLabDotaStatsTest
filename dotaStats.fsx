c99743c9f283d418ab0cc3642f2f9a5344010d4d
J2ghz <j2.00ghz@gmail.com>
2019-10-27 17:02:30 +0100

    data

--- dotaStats.fsx
+++ dotaStats.fsx
@@ -1,25 +1,12 @@
+open System.Globalization
 (*** hide ***)
 #load "packages/FsLab/Themes/DefaultWhite.fsx"
 #load "packages/FsLab/FsLab.fsx"
+
 (**
 
 Welcome to FsLab journal
 ========================
-
-FsLab journal is a simple Visual Studio template that makes it easy to do
-interactive data analysis using F# Interactive and produce HTML or PDF
-to document your research.
-
-Next steps
-----------
-
- * To see how things work, run `build run` from the terminal to start the journal
-   runner in the background (or hit **F5** in Visual Studio). Executing this
-   project will turn this F# script into a report.
-
- * To generate PDF from your experiments, you need to install `pdflatex` and
-   have it accessible in the system `PATH` variable. Then you can run
-   `build pdf` in the folder with this script (then check out `output` folder).
 
 Sample experiment
 -----------------
@@ -28,51 +15,41 @@ We start by referencing `Deedle` and `XPlot.GoogleCharts` libraries and then we
 load the contents of *this* file:
 *)
 
-(*** define-output:loading ***)
+open System
 open Deedle
 open System.IO
 open XPlot.GoogleCharts
+open FSharp.Data
 
-let file = __SOURCE_DIRECTORY__ + "/dotaStats.fsx"
-let contents = File.ReadAllText(file)
-printfn "Loaded '%s' of length %d" file contents.Length
-(*** include-output:loading ***)
+let toDateTime (timestamp:int) =
+  let start = DateTime(1970,1,1,0,0,0,DateTimeKind.Utc)
+  start.AddSeconds(float timestamp).ToLocalTime()
+
+type OpenDotaPlayerData = JsonProvider<"https://api.opendota.com/api/players/71050296/matches">
+
+let data = OpenDotaPlayerData.Load "https://api.opendota.com/api/players/71050296/matches"
+          |> Array.map (fun m -> m.StartTime |> toDateTime) 
+          |> Array.map (fun d -> d.Year,CultureInfo.CurrentCulture.DateTimeFormat.Calendar.GetWeekOfYear(d,CalendarWeekRule.FirstFourDayWeek,DayOfWeek.Monday))
+          |> Array.countBy id
+          |> Array.where (fun ((y,_),_) -> (y = 2019))
+          |> Array.map (fun ((y,w),c) -> (sprintf "%i/%i" y w , c))
+          |> Array.toList
 
 (**
 Now, we split the contents of the file into words, count the frequency of
 words longer than 3 letters and turn the result into a Deedle series:
 *)
-let words =
-  contents.Split(' ', '"', '\n', '\r', '*')
-  |> Array.filter (fun s -> s.Length > 3)
-  |> Array.map (fun s -> s.ToLower())
-  |> Seq.countBy id
+(*** define-output:series ***)
+let dataSeries =
+  data
   |> series
-(**
-We can take the top 5 words occurring in this tutorial and see them in a chart:
-*)
-(*** define-output:grid ***)
-words
-|> Series.sort
-|> Series.rev
-|> Series.take 7
-(*** include-it:grid ***)
+dataSeries
+(*** include-it:series ***)
+
 (**
 Finally, we can take the same 6 words and call `Chart.Column` to see them in a chart:
 *)
 (*** define-output:chart ***)
-words
-|> Series.sort
-|> Series.rev
-|> Series.take 7
+dataSeries
 |> Chart.Column
 (*** include-it:chart ***)
-
-(**
-Summary
--------
-An image is worth a thousand words:
-
-![](http://imgs.xkcd.com/comics/hofstadter.png)
-*)
-
