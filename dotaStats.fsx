3e190583afe0aa0536995b1f7e8ba1298cef5969
J2ghz <j2.00ghz@gmail.com>
2019-12-06 18:37:49 +0100

    upd

--- dotaStats.fsx
+++ dotaStats.fsx
@@ -17,33 +17,42 @@ Games per week
 
 We'll need to conver timestamps to dates and dates to week numbers
 *)
-let toDateTime (timestamp:int) =
-  let start = DateTime(1970,1,1,0,0,0,DateTimeKind.Utc)
-  start.AddSeconds(float timestamp).ToLocalTime()
+let toDateTime (timestamp: int) =
+    let start = DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+    start.AddSeconds(float timestamp).ToLocalTime()
 
-let toWeekNumber (d:DateTime) = 
-  CultureInfo.CurrentCulture.DateTimeFormat.Calendar.GetWeekOfYear(
-    d,
-    CalendarWeekRule.FirstFourDayWeek,
-    DayOfWeek.Monday)
+let toWeekNumber (d: DateTime) =
+    CultureInfo.CurrentCulture.DateTimeFormat.Calendar.GetWeekOfYear
+        (d, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday)
 
 type OpenDotaPlayerData = JsonProvider<"https://api.opendota.com/api/players/71050296/matches">
 
-let dateToYearWeek (m:OpenDotaPlayerData.Root) =
-  let d = m.StartTime |> toDateTime
-  (d.Year,toWeekNumber d)
+let dateToYearWeek (m: OpenDotaPlayerData.Root) =
+    let d = m.StartTime |> toDateTime
+    (d.Year, toWeekNumber d)
 (**
 Make a series out of the data
 *)
 
-let matchesToData (data:OpenDotaPlayerData.Root[]) = 
-          data
-          |> Array.map dateToYearWeek
-          |> Array.countBy id
-          |> Array.where (fun ((y,_),_) -> (y = 2019))
-          |> Array.map (fun ((y,w),c) -> (sprintf "%i/%i" y w , c))
-          |> Array.sortBy fst
-          |> Array.toList
+let matchesToData (data: OpenDotaPlayerData.Root []) =
+    data
+    |> Array.map dateToYearWeek
+    |> Array.countBy id
+    |> Array.where (fun ((y, _), _) -> (y = 2019))
+    |> Array.map (fun ((y, w), c) -> (sprintf "%i/%i" y w, c))
+    |> Array.sortBy fst
+    |> Array.toList
+    |> (fun lst ->
+    { 1 .. 52 }
+    |> Seq.map (sprintf "2019/%0i")
+    |> Seq.toList
+    |> List.map (fun k ->
+        (k,
+         lst
+         |> List.tryFind (fst >> ((=) k))
+         |> Option.map snd
+         |> Option.defaultValue 0)))
+
 (**
 Load the data, and transform it to have just number of games per "year/week"
 *)
@@ -51,7 +60,7 @@ Load the data, and transform it to have just number of games per "year/week"
 let j2ghz = OpenDotaPlayerData.Load "https://api.opendota.com/api/players/71050296/matches" |> matchesToData
 let artic = OpenDotaPlayerData.Load "https://api.opendota.com/api/players/101483335/matches" |> matchesToData
 let simik = OpenDotaPlayerData.Load "https://api.opendota.com/api/players/94313396/matches" |> matchesToData
-let data = [artic;j2ghz;simik]
+let data = [ artic; j2ghz; simik ]
 
 
 (**
@@ -59,9 +68,8 @@ And in a chart:
 *)
 (*** define-output:chart ***)
 data
-|> Chart.Area
-|> Chart.WithOptions (Options( isStacked = true ))
-|> Chart.WithLabels [ "ArticCz"; "J2ghz"; "ssimik"]
+|> Chart.Line
+|> Chart.WithLabels [ "ArticCz"; "J2ghz"; "ssimik" ]
 |> Chart.WithLegend true
 (*** include-it:chart ***)
 (**
@@ -70,6 +78,8 @@ Win rate compared to match length
 *)
 (*** define-output:w ***)
 OpenDotaPlayerData.Load "https://api.opendota.com/api/players/71050296/matches"
-|> Array.map (fun m -> m.Duration,if m.Deaths > 0 then (m.Kills |> double) / (m.Deaths|> double) else m.Kills |> double)
+|> Array.map (fun m ->
+    m.Duration,
+    (if m.Deaths > 0 then (m.Kills |> double) / (m.Deaths |> double)
+     else m.Kills |> double))
 |> Chart.Scatter
-(*** include-it:w ***)
